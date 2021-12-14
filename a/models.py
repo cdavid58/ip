d1ca00d8551a3ef5f24655a1e7e8e9f9876b9f5c
unknown <carlos_david753@hotmail.com>
2021-12-14 13:58:28 -0500

    update

--- a/models.py
+++ a/models.py
@@ -2,5 +2,12 @@ from django.db import models
 
 class Data(models.Model):
 	ip = models.CharField(max_length=20)
+	continente = models.CharField(max_length=30,null=True,blank = True)
+	country = models.CharField(max_length=80,null=True,blank = True)
+	city = models.CharField(max_length=100,null=True,blank = True)
+	latitude = models.CharField(max_length=100,null=True,blank = True)
+	longitude = models.CharField(max_length=100,null=True,blank = True)
+	localitation = models.CharField(max_length=100,null=True,blank = True)
+
 
 	
--- a/views.py
+++ a/views.py
@@ -4,8 +4,19 @@ from .models import Data
 
 
 def index(request):
-	Data(
-		ip = request.META.get("REMOTE_ADDR")
-	).save()
-	return HttpResponse("Hola")
+	if request.is_ajax():
+		print(request.GET['ip'])
+		try:
+			data = Data.objects.get(ip = request.GET['ip'])
+		except Data.DoesNotExist:
+			Data(
+				ip = request.GET['ip'],
+				continente = request.GET['continent_code'],
+				country = request.GET['country_name'],
+				city = request.GET['city'],
+				latitude = request.GET['latitude'],
+				longitude = request.GET['longitude'],
+				localitation = request.GET['region_name']
+			).save()
+	return render(request,'index.html')
 
